2c29f9f7877ccadab4290e868819c121f4f7b6fe
mrbrunotte <mrbrunotte@gmail.com>
2019-09-07 11:44:25 +0200

    Insert value into table

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -6,11 +6,13 @@ cursor = connection.cursor()
 
 
 try:
-    # create a table called Friends
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS
-                    Friends(name char(20), age int, DOB datetime);""")
-    for row in cursor:
-        print(row)
+    # For readability the values will be stored in a TULP which we call row
+    row = ("Stefan", 47, "1972-02-17 01:15:45")
+    # to insert a single row into a table, use the standard SQL INSERT command using a cursor
+    # The cursor unpacks the TULP and inserts the three VALUE into the INSERT statement
+    # %s, %s, %s represents the three items in the TULP 
+    cursor.execute("INSERT INTO Friends VALUES(%s, %s, %s);", row)
+    connection.commit()
 
 finally:
     # close the connection
