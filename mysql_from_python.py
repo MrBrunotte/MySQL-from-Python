80aa6dd4f31fd1d0c183efb1f5d8fe218728722f
mrbrunotte <mrbrunotte@gmail.com>
2019-09-07 12:10:40 +0200

    Update a row

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -6,10 +6,7 @@ cursor = connection.cursor()
 
 
 try:
-    rows = [("Stefan", 47, "1972-02-17 01:15:45"),
-            ("Rebecca", 47, "1972-04-11 03:45:25"),
-            ("Fred", 19, "2000-10-05 15:17:50")]
-    cursor.executemany("INSERT INTO Friends VALUES(%s, %s, %s);", rows)
+    cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Stefan';")
     connection.commit()
 
 finally:
