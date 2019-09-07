9fb599378c78bd53db827169eaa23c66a14513b1
mrbrunotte <mrbrunotte@gmail.com>
2019-09-07 10:50:54 +0200

    Add DictCursor and commands.txt file

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -1,14 +1,16 @@
 import pymysql
 
 connection = pymysql.connect("localhost", "root", "", "chinook")
-cursor = connection.cursor()
+cursor = connection.cursor(pymysql.cursors.DictCursor) 
+# The advantage of using the DictCursor is that the rows now include the column names.
 
-sql_query = "SELECT * FROM Artist;"
 
 try:
+    sql_query = "SELECT * FROM Genre;"
+    # the curser is the object that we use to execute queries
     cursor.execute(sql_query)
-    result = cursor.fetchall()
-    print(result)
+    for row in cursor:
+        print(row)
 
 finally:
     # close the connection
