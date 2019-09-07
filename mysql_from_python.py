3ffbc90343066caa58d625cffc6a3db67a5da9a2
mrbrunotte <mrbrunotte@gmail.com>
2019-09-07 12:01:17 +0200

    Execute many

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -2,16 +2,14 @@ import datetime
 import pymysql
 
 connection = pymysql.connect("localhost", "root", "", "chinook")
-cursor = connection.cursor() 
+cursor = connection.cursor()
 
 
 try:
-    # For readability the values will be stored in a TULP which we call row
-    row = ("Stefan", 47, "1972-02-17 01:15:45")
-    # to insert a single row into a table, use the standard SQL INSERT command using a cursor
-    # The cursor unpacks the TULP and inserts the three VALUE into the INSERT statement
-    # %s, %s, %s represents the three items in the TULP 
-    cursor.execute("INSERT INTO Friends VALUES(%s, %s, %s);", row)
+    rows = [("Stefan", 47, "1972-02-17 01:15:45"),
+            ("Rebecca", 47, "1972-04-11 03:45:25"),
+            ("Fred", 19, "2000-10-05 15:17:50")]
+    cursor.executemany("INSERT INTO Friends VALUES(%s, %s, %s);", rows)
     connection.commit()
 
 finally:
